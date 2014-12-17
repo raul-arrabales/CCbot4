3f73a1f849cc1cc90be468880267df3a943ee10d
raul.arrabales <raul.arrabales@28APO0679.altran.es>
2014-12-17 17:36:18 +0100

    Initial commit

---
+++ src/ccbot4/CCbot4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccbot4;
+
+/**
+ *
+ * @author raul.arrabales
+ */
+public class CCbot4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hi");
+    }
+    
+}
